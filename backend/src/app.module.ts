d22a7974b0bdfec282f3365bcdeb75c3dcbdbb20
Kanon Snow <kanonchikcrypto@gmail.com>
2026-03-10 15:16:40 +0100

    openai setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { TelegramModule } from './telegram/telegram.module';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TelegramModule } from './telegram/telegram.module';
     }),
     TelegramModule,
     PrismaModule,
+    OpenaiModule,
   ],
   providers: [PrismaService],
 })
