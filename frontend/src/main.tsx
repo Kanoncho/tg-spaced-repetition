2098db36e5b3d44c09d486da282ab79df1c30024
Kanon Snow <kanonchikcrypto@gmail.com>
2026-03-10 13:34:10 +0100

    eruda setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { init, miniApp } from '@telegram-apps/sdk-react'
+import eruda from 'eruda'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from './provider'
 
 init()
 miniApp.mountSync()
+eruda.init()
 
 const root = document.getElementById('root')
 
