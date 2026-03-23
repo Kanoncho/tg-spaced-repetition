a96b307780d0c5a8519578bec07e78cea4d29f50
Kanon Snow <kanonchikcrypto@gmail.com>
2026-03-23 17:43:02 +0100

    tanstack hook fix

--- frontend/src/api/hooks/note/useNoteCards.ts
+++ frontend/src/api/hooks/note/useNoteCards.ts
@@ -3,7 +3,7 @@ import { NoteService } from '../../services/note.service'
 
 export const useNoteCards = (id: string) => {
 	return useQuery({
-		queryKey: ['cards'],
+		queryKey: ['cards, id'],
 		queryFn: () => NoteService.getNoteCards(id),
 	})
 }
