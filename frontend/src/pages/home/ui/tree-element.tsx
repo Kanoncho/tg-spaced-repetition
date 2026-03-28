d930a5c781cc2f581aea5e1f8060227cff4c93d5
Kanon Snow <kanonchikcrypto@gmail.com>
2026-03-28 17:14:25 +0100

    ui tabs styling

--- frontend/src/pages/home/ui/tree-element.tsx
+++ frontend/src/pages/home/ui/tree-element.tsx
@@ -13,7 +13,7 @@ export function TreeElement({ name, type, link }: Props) {
 			className='relative w-full rounded-lg py-4 flex items-center overflow-hidden justify-center gap-1 bg-background-secondary'
 		>
 			<div
-				className={`absolute top-0 left-0 h-full w-1.5 ${type === 'folder' ? 'bg-amber-600' : 'bg-fuchsia-600'}`}
+				className={`absolute top-0 left-0 h-full w-1.5 ${type === 'folder' && 'bg-button'}`}
 			></div>
 			<span>{name}</span>
 		</Link>
--- frontend/src/shared/ui/tabs.tsx
+++ frontend/src/shared/ui/tabs.tsx
@@ -1,10 +1,17 @@
+import { NotebookText, Settings } from 'lucide-react'
 import { Link } from 'react-router'
 
 export function Tabs() {
 	return (
-		<section className='p-2 flex gap-2 items-center fixed bottom-5 left-1/2 -translate-x-1/2 bg-amber-900'>
-			<Link to='/configuration'>Configuration</Link>
-			<Link to='/'>Main</Link>
+		<section className='py-2 px-8 flex gap-12 items-center fixed bottom-10 left-1/2 -translate-x-1/2 rounded-full bg-background-secondary'>
+			<Link to='/configuration' className='flex flex-col gap-1 items-center'>
+				<Settings width={32} height={32} />
+				<span className='text-xs'>Config</span>
+			</Link>
+			<Link to='/' className='flex flex-col gap-1 items-center'>
+				<NotebookText width={32} height={32} />
+				<span className='text-xs'>Notes</span>
+			</Link>
 		</section>
 	)
 }
